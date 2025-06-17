ec54cac1e2d606ae210b201633b64593b6346711
Carlos Salas Alpizar <carlos.salas5518@gmail.com>
2025-06-16 21:31:33 -0600

    Revert "Semana 6, se agrego productos, modificacion y eliminacion"
    This reverts commit ba727bb478a1bfaeb0fb06314bb211226bbdc958.

--- src/main/java/com/tienda/services/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/services/impl/CategoriaServiceImpl.java
@@ -1,44 +1,31 @@
+
 package com.tienda.services.impl;
 
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
-import com.tienda.services.CategoriaService;
 import java.util.List;
+import com.tienda.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceImpl implements CategoriaService {
-
+public class CategoriaServiceImpl implements CategoriaService{
+    
     @Autowired
     private CategoriaDao categoriaDao;
-
+    
     @Override
-    @Transactional(readOnly = true)
-    public List<Categoria> getCategorias(boolean activos) {
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activos){
         var lista = categoriaDao.findAll();
-        if (activos) {
+        if(activos){
             lista.removeIf(e -> !e.isActivo());
+  
         }
+        
         return lista;
+          
     }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Categoria getCategoria(Categoria categoria) {
-        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
-    }
-
-    @Override
-    @Transactional
-    public void save(Categoria categoria) {
-        categoriaDao.save(categoria);
-    }
-
-    @Override
-    @Transactional
-    public void delete(Categoria categoria) {
-        categoriaDao.delete(categoria);
-    }
+        
 }
